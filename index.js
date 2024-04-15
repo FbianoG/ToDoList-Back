b465bc95968bf2029cc737ecf4abc768cb56c276
Fabiano Gonçalves <boriscasoy123@gmail.com>
2024-04-15 13:06:09 -0300

    asdasdas

--- index.js
+++ index.js
@@ -2,16 +2,12 @@ const express = require('express')
 const router = require("./src/routes/router.js")
 const DataBase = require("./src/dataBase/db.js")
 const cors = require('cors')
-const corsOptions = {
-    origin: 'http://localhost:5173',
-    methods: ['GET', 'POST', 'OPTIONS', 'PUT', 'DELETE']
-  }
 
 
 
 const app = express()
 const port = 3000
-app.use(cors(corsOptions))
+app.use(cors())
 
 
 
