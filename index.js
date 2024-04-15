2d86d3cc8b50a483980eebb5bbb025616c3959a3
Fabiano Gonçalves <boriscasoy123@gmail.com>
2024-04-15 11:43:07 -0300

    a

--- index.js
+++ index.js
@@ -1,22 +1,22 @@
 const express = require('express')
 const router = require('./src/routes/router.js')
-const DataBase = require('./src/dataBase/db.js')
-const cors = require('cors')
+const DataBase = require("./src/dataBase/db.js")
 
+const cors = require('cors')
 const app = express()
 const port = 3000
 
-app.use(cors())
+
+app.use(cors());
+
+
 app.use(express.static('public'))
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 app.use(router)
 
+DataBase.connectDataBase()
 
-app.listen(port, async () => {
-    await DataBase.connectDataBase()
-    console.log(`Servidor funcionando: http://localhost:` + port)
+app.listen(port, () => {
+    console.log(`Servidor funcionando na porta:`, port)
 })
-
-
-
