1d5d0ffd323899d0fa53729a5542972873d0c3b5
Fabiano Gonçalves <boriscasoy123@gmail.com>
2024-04-15 14:54:38 -0300

    123

--- index.js
+++ index.js
@@ -1,8 +1,7 @@
 const express = require('express')
 const router = require('./src/routes/router.js')
 const DataBase = require('./src/dataBase/db.js')
-const socketIo = require('socket.io');
-const http = require('http');
+
 
 
 const cors = require('cors')
@@ -10,31 +9,6 @@ const cors = require('cors')
 
 const app = express()
 const port = 3000
-
-const server = http.createServer(app);
-const io = socketIo(server);
-
-
-
-io.on('connection', (socket) => {
-    console.log('Novo cliente conectado');
-
-    // Exemplo: enviar uma mensagem para o cliente conectado
-    socket.emit('message', 'Bem-vindo ao servidor!');
-    
-    // Exemplo: ouvir uma mensagem do cliente
-    socket.on('clientMessage', (data) => {
-        console.log('Mensagem recebida do cliente:', data);
-    });
-
-    // Exemplo: enviar uma mensagem para todos os clientes
-    io.emit('broadcast', 'Mensagem para todos os clientes');
-});
-
-
-
-
-
 
 
 app.use(cors())
