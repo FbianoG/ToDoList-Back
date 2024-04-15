c7f7484e1c042255046f23f351b0b543ac6f3036
Fabiano Gonçalves <boriscasoy123@gmail.com>
2024-04-15 11:49:15 -0300

    sd

--- index.js
+++ index.js
@@ -1,19 +1,12 @@
 const express = require('express')
-const router = require('./src/routes/router.js')
+const router = require('./src/routes//router.js')
 const DataBase = require("./src/dataBase/db.js")
 
 const cors = require('cors')
 const app = express()
 const port = 3000
-const corsOptions = {
-    origin: '*',
-    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
-    credentials: true,
-    optionsSuccessStatus: 204,
-    exposedHeaders: 'Authorization',
-}
 
-app.use(cors(corsOptions));
+app.use(cors());
 
 
 app.use(express.static('public'))
