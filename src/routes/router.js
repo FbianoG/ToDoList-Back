497025e001c0c6230944be8aec208b849763b8f0
Fabiano Gonçalves <boriscasoy123@gmail.com>
2024-04-15 12:27:16 -0300

    c

--- src/routes/router.js
+++ src/routes/router.js
@@ -8,5 +8,8 @@ router.post("/getTasks", jwt.verifyToken, control.getTasks)
 router.post("/createTask", jwt.verifyToken, control.createTask)
 router.post("/deleteTask", jwt.verifyToken, control.deleteTask)
 router.post("/updateTasks", jwt.verifyToken, control.updateTasks)
+router.get("/", (req, res) => {
+    res.json({ message: "Olá, mundo!" })
+})
 
 module.exports = router
